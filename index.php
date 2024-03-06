fc1341973d9ba62a6cecbbc89b446e476c44e216
maverick-farhan <mdfarhanofficial@gmail.com>
2024-03-06 13:55:55 +0530

    Updated

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Open Source|Certificate Builder</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="header">
+        <h1 class="logo">Certificate Builder <i class="fa-solid fa-award"></i></h1>
+        <ul class="nav-list">
+            <li class="nav-items build"><a href="">Build Certificate</a></li>
+            <li class="nav-items dark"><i class="fa-regular fa-moon" id="moonIcon"></i></li>
+            <li class="nav-items github"><a href=""><i class="fa-brands fa-github"></i></a></li>
+        </ul>
+    </nav>
+    <main class="hero-section">
+        <a href="https://www.buymeacoffee.com/mdfarhan"></a>
+    </main>
+    <script src="./js/index.js"></script>
+</body>
+</html>
---
+++ js/index.js
@@ -0,0 +1,5 @@
+let moonIcon = document.getElementById('moonIcon');
+moonIcon.addEventListener('click', (e)=>{
+    // e.target.classList.add('fa-regular fa-sun');
+    console.log(e.target);
+})
