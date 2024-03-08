3aa810f2e290d67145503e9806e0a8c4be9e2c3f
maverick-farhan <mdfarhanofficial@gmail.com>
2024-03-08 16:58:21 +0530

    Added

--- index.php
+++ index.php
@@ -6,19 +6,41 @@
     <title>Open Source|Certificate Builder</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
 </head>
 <body>
     <nav class="header">
         <h1 class="logo">Certificate Builder <i class="fa-solid fa-award"></i></h1>
         <ul class="nav-list">
             <li class="nav-items build"><a href="">Build Certificate</a></li>
-            <li class="nav-items dark"><i class="fa-regular fa-moon" id="moonIcon"></i></li>
-            <li class="nav-items github"><a href=""><i class="fa-brands fa-github"></i></a></li>
+            <li id="moon" class="nav-items dark"><i class="fa-regular fa-moon" id="moonIcon"></i></li>
+            <li id="github" class="nav-items github"><a href="https://github.com/maverick-farhan/certificate-builder" target="_blank"><i class="fa-brands fa-github"></i></a></li>
         </ul>
     </nav>
+    
+    <div class="menubar-mobile">
+        <ul class="nav-list">
+            <li class="nav-items dark"><i class="fa-regular fa-moon" id="moonIcon"></i></li>
+            <li class="nav-items github"><a href="https://github.com/maverick-farhan/certificate-builder" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+        </ul>
+    </div>
+    
     <main class="hero-section">
-        <a href="https://www.buymeacoffee.com/mdfarhan"></a>
+        <div class="container">
+            <h1 class="title">Build your custom certificates in just few clicks and key strokes.</h1>
+            <div class="image"><img src="./asset/certificate-4.jpeg" alt="certificate-demo" /></div>
+        </div>
+            <button type="button" class="build"><a href="#">Build Certificate</a></button>
     </main>
+    <footer>
+            <h1>Build With &#x1F90E;</h1>
+            <h3>“ If you do like this project. Kindly consider supporting me it will help me a lot to turn ideas into products. Happy hacking.”</h3>
+           <div class="support-link">
+           <a class="support" href="https://www.buymeacoffee.com/mdfarhan" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;"></a>
+           </div> 
+            <h4>&copy; Md Farhan 2024</h4>
+    </footer>
+
     <script src="./js/index.js"></script>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,5 +1,25 @@
 let moonIcon = document.getElementById('moonIcon');
+const github = document.getElementById('github');
+const mode = document.getElementById('moon');
+const menu_mobile = document.querySelector('.menubar-mobile');
+
+
+
+window.addEventListener('resize', ()=>{
+if(screen.width < 700){
+        mode.style.display="none";
+        github.style.display="none";
+        menu_mobile.style.visibility="visible";
+}
+else{
+        mode.style.display="block";
+        github.style.display="block";
+        menu_mobile.style.visibility="hidden";
+
+}
+},true);
+
 moonIcon.addEventListener('click', (e)=>{
-    // e.target.classList.add('fa-regular fa-sun');
-    console.log(e.target);
-})
+        e.target.classList.remove('moonIcon');
+        e.target.classList.toggle("fa-sun");
+});
