ce1ab9c3872c3529e99ccb8335a2072012e6ba1e
maverick-farhan <mdfarhanofficial@gmail.com>
2024-03-19 16:39:56 +0530

    Updated

--- js/index.js
+++ js/index.js
@@ -1,16 +1,12 @@
-let moonIcon = document.getElementById('moonIcon');
-const github = document.getElementById('github');
-const mode = document.getElementById('moon');
+const github = document.querySelector('.github');
 const menu_mobile = document.querySelector('.menubar-mobile');
 
 const mobileSize = ()=>{
 if(screen.width < 700){
-        mode.style.display="none";
         github.style.display="none";
         menu_mobile.style.visibility="visible";
 }
 else{
-        mode.style.display="block";
         github.style.display="block";
         menu_mobile.style.visibility="hidden";
 
@@ -19,8 +15,3 @@ else{
 if(window.screen.width<700){
         mobileSize();
 }
-
-moonIcon.addEventListener('click', (e)=>{
-        e.target.classList.remove('moonIcon');
-        e.target.classList.toggle("fa-sun");
-});
--- logic/builder.php
+++ logic/builder.php
@@ -10,7 +10,7 @@
     <title>Builder Certificate</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="builder.css">
+    <link rel="stylesheet" href="../css/builder.css">
 </head>
 <body>
 <nav class="header">
@@ -21,18 +21,32 @@
 
 <div class="side-bar certificate-options-container">
     <h1 class="title">Choose Your Design</h1>
-    <div class="image-container">
-        <img src="../asset/certificate-1.jpeg" alt="">
-        <img src="../asset/certificate-2.jpeg" alt="">
-        <img src="../asset/certificate-3.jpeg" alt="">
-        <img src="../asset/certificate-4.jpeg" alt="">
-        <img src="../asset/certificate-10.jpeg" alt="">
-        <img src="../asset/certificate-6.jpeg" alt="">
-        <img src="../asset/certificate-8.jpeg" alt="">
-        <img src="../asset/certificate-7.jpeg" alt="">
+    <div class="img-container">
+        <img class="img active" src="../asset/certificate-1.jpeg" alt="">
+        <img class="img" src="../asset/certificate-2.jpeg" alt="">
+        <img class="img" src="../asset/certificate-3.jpeg" alt="">
+        <img class="img" src="../asset/certificate-4.jpeg" alt="">
+        <img class="img" src="../asset/certificate-10.jpeg" alt="">
+        <img class="img" src="../asset/certificate-6.jpeg" alt="">
+        <img class="img" src="../asset/certificate-8.jpeg" alt="">
+        <img class="img" src="../asset/certificate-7.jpeg" alt="">
         <div class="msg">That's it folks ♡</div>
+		<div class="navigation">
+			<div class="btn active"></div>
+			<div class="btn"></div>
+			<div class="btn"></div>
+			<div class="btn"></div>
+			<div class="btn"></div>
+			<div class="btn"></div>
+			<div class="btn"></div>
+			<div class="btn"></div>
+		</div>
     </div>
+
 </div>
+
+
+
 
 <div class="form-container">
     <h1 class="title">Fill Required details</h1>
@@ -45,7 +59,7 @@
             <input type="text" name="course" id="course" placeholder="Course" />
         </label>
         <label for="date"><p>Date of Completion </p>
-            <?php echo '<input id="date" x-mask="99/99/9999" placeholder="MM/DD/YYYY" value="date"/>'; ?>
+            <input id="date" placeholder="DD/MM/YYYY" value=""/>
         </label>
 
         <input type="submit" value="Built it" >
@@ -55,11 +69,47 @@
 
 </main>
 
+<script>
+// Certificates Slider
+const slides = document.querySelectorAll('.img');
+const btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+const manualSlide = (manual)=>{
+    slides.forEach((slide)=>{
+        slide.classList.remove('active');
+    });
+    btns.forEach((btn)=>{
+        btn.classList.remove('active');
+    })
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+btns.forEach((btn,i)=>{
+    btn.addEventListener('click',()=>{
+        manualSlide(i);
+        currentSlide = i;
+    })
+})
+
+let dateInput = document.querySelector('#date');
+let newDate = new Date();
+let dd = String(newDate.getDate()).padStart(2,'0');
+let mm = String(newDate.getMonth()+1).padStart(2,'0');
+let yyyy = newDate.getFullYear();
+let resultDate = dd + '/'+mm + '/' + yyyy;
+dateInput.value = resultDate; 
+
+
+slides.forEach((slide)=>{
+    slide.addEventListener('click',()=>{
+        slide.style.outline = '2px solid green';
+    })
+})
+
     
-<!-- Alpine Plugins -->
-<script src="https://cdn.jsdelivr.net/npm/@alpinejs/mask@3.x.x/dist/cdn.min.js"></script>
- 
-<!-- Alpine Core -->
-<script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
+</script> 
 </body>
 </html>
